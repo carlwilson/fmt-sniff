4e57ddb471a6ff157411bd3e714b3b3ebd66b0b7
Carl Wilson <carl@openplanetsfoundation.org>
2017-02-11 15:32:51 +0000

    FEAT - S3 API Access - added S3 bucket access methods; - performed full working test of corpus download; - integrated blobstore with S3 API corpus; and - pylint compliance progress:   * tidied imports;   * shortened over-long lines;   * spacing and indentation conventions;   * added DocStrings; and   * no more camel case method names.

--- corptest/const.py
+++ corptest/const.py
@@ -9,26 +9,31 @@
 # License, Version 3. See the text file "COPYING" for further details
 # about the terms of this license.
 #
-# Constants used across corptest modules
+"""Constants used across corptest modules"""
 
 import datetime
 
 SOURCE_ROOT = 'http://rdss-test-data.s3-eu-west-1.amazonaws.com/'
-BLOB_STORE_ROOT = '/tmp/jisc/blobs/'
+BLOB_STORE_ROOT = '/home/cfw/arch/data/samp/JISC/blobstore/'
+JISC_BUCKET = 'testdata.researchdata.alpha.jisc.ac.uk'
 
-class AS3EleFields:
+
+class AS3EleFields(object):
+    """Field names and default for AS3 Element type"""
     KEY = 'key'
     ETAG = 'etag'
     SIZE = 'size'
     MODIFIED = 'last_modified'
     DEFAULT = {
-        KEY      : '',
-        ETAG     : '',
-        SIZE     : 0,
-        MODIFIED : datetime.date.today()
+        KEY: '',
+        ETAG: '',
+        SIZE: 0,
+        MODIFIED: datetime.date.today()
     }
 
-class AS3Tags:
+
+class AS3Tags(object):
+    """XML tag names for AS3 element"""
     # Parent tags
     RESULTS = 'ListBucketResult'
     CONTENTS = 'Contents'
@@ -39,10 +44,12 @@ class AS3Tags:
     ETAG = 'ETag'
     SIZE = 'Size'
 
-class AS3TagMap:
+
+class AS3TagMap(object):
+    """Lookup tag map of XML tag values"""
     LOOKUP = {
-        AS3Tags.KEY      : AS3EleFields.KEY,
-        AS3Tags.MODIFIED : AS3EleFields.MODIFIED,
-        AS3Tags.ETAG     : AS3EleFields.ETAG,
-        AS3Tags.SIZE     : AS3EleFields.SIZE
+        AS3Tags.KEY: AS3EleFields.KEY,
+        AS3Tags.MODIFIED: AS3EleFields.MODIFIED,
+        AS3Tags.ETAG: AS3EleFields.ETAG,
+        AS3Tags.SIZE: AS3EleFields.SIZE
     }
