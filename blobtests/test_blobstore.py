a640faec736961827e2e96da99ec547ca2e1e6b2
Carl Wilson <carl@openplanetsfoundation.org>
2017-04-02 23:20:04 +0100

    REFACT - Refactored BlobStore - added a second persistent blobstore module; - added from_file() and from_string() methods to ByteSequence; and - created full set of unit tests for ByteSequence.

--- blobtests/test_blobstore.py
+++ blobtests/test_blobstore.py
@@ -8,17 +8,70 @@
 # This code is distributed under the terms of the GNU General Public
 # License, Version 3. See the text file "COPYING" for further details
 # about the terms of this license.
+""" Tests for the classes in blobstore.py. """
+import os.path
 import unittest
+
 from corptest.blobstore import ByteSequence
+from corptest.utilities import ObjectJsonEncoder
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class ByteSequenceTestCase(unittest.TestCase):
+    """ Test cases for the ByteSequence class and methods. """
     def setUp(self):
-        self.default_byte_seq = ByteSequence()
+        """ Set up default instance """
+        self.default_test = ByteSequence(ByteSequence.EMPTY_SHA1, 0)
+        self.def_inst = ByteSequence.default_instance()
 
-    def test_default_size(self):
-        assert self.default_byte_seq.get_size() == 0, 'Default size should be zero'
+    def test_default_instance(self):
+        """ Test case for default instance. """
+        assert self.default_test == self.def_inst, \
+        'Test default should be equal to default instance'
 
-    def test_default_sha1(self):
-        assert self.default_byte_seq.get_sha1() == 'da39a3ee5e6b4b0d3255bfef95601890afd80709'
+    def test_from_empty_file(self):
+        """ Test case for ByteSequence.from_file() method. """
+        empty_test_path = os.path.join(THIS_DIR, 'empty')
+        test_byte_seq = ByteSequence.from_file(empty_test_path)
+        assert test_byte_seq == self.def_inst, \
+        'Test from empty file should be equal to default instance'
+
+    def test_from_file(self):
+        """ Test case for ByteSequence.from_file() method. """
+        notempty_test_path = os.path.join(THIS_DIR, 'notempty')
+        test_byte_seq = ByteSequence.from_file(notempty_test_path)
+        assert test_byte_seq != self.def_inst, \
+        'Test from file should NOT be equal to default instance'
+
+    def test_from_empty_string(self):
+        """ Test case for ByteSequence.from_string() method. """
+        test_byte_seq = ByteSequence.from_string('')
+        assert test_byte_seq == self.def_inst, \
+        'Test from empty file should be equal to default instance'
+
+    def test_from_string(self):
+        """ Test case for ByteSequence.from_string() method. """
+        test_byte_seq = ByteSequence.from_string('notempty')
+        assert test_byte_seq != self.def_inst, \
+        'Test from file should NOT be equal to default instance'
+
+    def test_json_empty(self):
+        """ Test case for ByteSequence.json_decode() method. """
+        encoder = ObjectJsonEncoder()
+        json_string = encoder.default(self.def_inst)
+        test_byte_seq = ByteSequence.json_decode(json_string)
+        assert test_byte_seq == self.def_inst, \
+        'Test from JSON should be equal to default instance'
+
+    def test_json_not_empty(self):
+        """ Test case for ByteSequence.json_decode() method. """
+        encoder = ObjectJsonEncoder()
+        notempty_test_path = os.path.join(THIS_DIR, 'notempty')
+        byte_seq = ByteSequence.from_file(notempty_test_path)
+        json_string = encoder.default(byte_seq)
+        test_byte_seq = ByteSequence.json_decode(json_string)
+        assert test_byte_seq != self.def_inst, \
+        'Test from JSON should be equal to default instance'
+
 if __name__ == "__main__":
     unittest.main()
--- corptest/blobstore.py
+++ corptest/blobstore.py
@@ -18,7 +18,8 @@ import os
 
 from const import BLOB_STORE_ROOT
 from format_tools import Sha1Lookup
-from utilities import ObjectJsonEncoder, only_files, hashfile, create_dirs, hash_copy_file
+from utilities import hashfile, hashstring, hash_copy_file
+from utilities import ObjectJsonEncoder, only_files, create_dirs
 
 class ByteSequence(object):
     """Key attributes for all byte sequences, i.e. arbitary blobs of data."""
@@ -35,6 +36,16 @@ class ByteSequence(object):
         """Return the size of the ByteSequence in bytes."""
         return self.size
 
+    def __eq__(self, other):
+        """ Define an equality test for ByteSequence """
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        return False
+
+    def __ne__(self, other):
+        """ Define an inequality test for ByteSequence """
+        return not self.__eq__(other)
+
     def __str__(self):
         ret_val = []
         ret_val.append("ByteSequence : [sha1=")
@@ -50,6 +61,22 @@ class ByteSequence(object):
         return ByteSequence()
 
     @classmethod
+    def from_file(cls, source_path):
+        """ Creates a new ByteStream instance from the supplied file path. """
+        if not os.path.isfile(source_path):
+            raise IOError(errno.ENOENT, os.strerror(errno.ENOENT), source_path)
+        with open(source_path, 'rb') as src:
+            sha1 = hashfile(src, hashlib.sha1())
+        size = os.path.getsize(source_path)
+        return cls(sha1, size)
+
+    @classmethod
+    def from_string(cls, source):
+        """ Creates a new ByteStream instance from the supplied string. """
+        sha1 = hashstring(source, hashlib.sha1())
+        return cls(sha1, len(source))
+
+    @classmethod
     def json_decode(cls, obj):
         """ Custom JSON decoder for ByteSequence. """
         cls_name = '__{}__'.format(cls.__name__)
@@ -61,13 +88,10 @@ class ByteSequence(object):
 class BlobStore(object):
     """ Hash identified store for binary objects. """
     __blobpath__ = 'blobs/'
-    __metapath__ = 'meta/'
-    __blobmeta__ = __metapath__ + 'blob.json'
-    def __init__(self, root, persist=True):
+    def __init__(self, root):
         self.root = root
         self.size = 0
         self.blobs = collections.defaultdict(dict)
-        self.is_persistent = persist
         self.initialise()
 
     def get_blob(self, sha1):
@@ -84,18 +108,27 @@ class BlobStore(object):
             self.size = self.calculate_size()
         return self.size
 
-    def is_store_persistent(self):
-        """ Return true if the blob store is persistent. """
-        return self.is_persistent
+    def get_root(self):
+        """Get the root directory for the BLOB store"""
+        return self.root
+
+    def replace_blobs(self, blobs):
+        """ Loads the datacentre lookup dictionary from fp (a read() supporting
+        file like object)."""
+        self.blobs.clear()
+        self.size = 0
+        self.blobs = blobs
 
     def add_file(self, path, sha1=None):
         """ Adds file at path to corpus and returns the sha1. """
-        if not os.path.isfile(path):
-            raise IOError(errno.ENOENT, os.strerror(errno.ENOENT), path)
-        if sha1 is None:
-            with open(path, 'rb') as src:
-                sha1 = hashfile(src, hashlib.sha1())
-        if not sha1 in self.blobs.keys():
+        byte_sequence = ByteSequence.from_file(path)
+        if sha1 is not None and sha1 != byte_sequence.get_sha1():
+            raise IOError(errno.ENOENT, os.strerror(errno.ENOENT),
+                          'Supplied hash {} does not \
+                           match {} calculated from {}.'.format(sha1,
+                                                                byte_sequence.get_sha1(),
+                                                                path))
+        if sha1 not in self.blobs.keys():
             dest_path = self.get_blob_path(sha1)
             with open(path, 'rb') as src:
                 with open(dest_path, 'w+') as dest:
@@ -103,9 +136,9 @@ class BlobStore(object):
             if calc_sha1 != sha1:
                 raise IOError(errno.ENOENT, os.strerror(errno.ENOENT),
                               'SHA1 failure copying {}.'.format(path))
-            size = os.path.getsize(path)
-            self.blobs.update({calc_sha1 : ByteSequence(calc_sha1, size)})
-        return sha1
+            self.blobs.update({byte_sequence.get_sha1() : byte_sequence})
+
+        return byte_sequence
 
     def hash_check(self):
         """Performs a hash check of all BLOBs in the store"""
@@ -117,10 +150,6 @@ class BlobStore(object):
             if to_check[0] != to_check[1]:
                 print "Digest mis-maatch for file " + blob_root + to_check[0] + \
                 ", calculated: " + to_check[1]
-
-    def get_root(self):
-        """Get the root directory for the BLOB store"""
-        return self.root
 
     def get_blob_path(self, sha1):
         """Returns the file path of a the BLOB called blob_name"""
@@ -140,51 +169,14 @@ class BlobStore(object):
         Populates lookup table and saves to persist_to if persist_to doesn't exist.
         """
         self.check_and_create_root()
-
-        if not self.is_persistent:
-            # No persist requested, populate the dictionary
-            self.reload_blobs()
-            return
-
-        persist_to = self.get_blob_meta_path()
-        if os.path.isfile(persist_to):
-            # Persistence file exists, load the dictionary
-            with open(persist_to, 'r') as lookup_file:
-                self.load(lookup_file)
-        else:
-            self.reload_blobs()
-            with open(persist_to, 'w+') as lookup_file:
-                self.save(lookup_file)
-
-    def get_blob_meta_path(self):
-        """ Returns the full path of the Blob metadata file. """
-        return self.root + self.__class__.__blobmeta__
-
-    def persist(self):
-        """ Persists the current state of the internal blobstore dictionary
-        to the default location, overwriting the existing state.
-        """
-        persist_to = self.get_blob_meta_path()
-        with open(persist_to, 'w+') as lookup_file:
-            self.save(lookup_file)
-
-    def save(self, dest):
-        """ Serialise the datacentre lookup dictionary to fp (a write() supporting
-        file-like object). """
-        json.dump(self.blobs, dest, cls=ObjectJsonEncoder)
-
-    def load(self, src):
-        """ Loads the datacentre lookup dictionary from fp (a read() supporting
-        file like object)."""
-        self.blobs.clear()
-        self.size = 0
-        self.blobs = json.load(src, object_hook=ByteSequence.json_decode)
+        self.reload_blobs()
+        return
 
     def check_and_create_root(self):
         """ Checks that the root dirs for the BlobStore exist and makes
         them if missing.
         """
-        dirs = ['', self.__blobpath__, self.__metapath__]
+        dirs = ['', self.__blobpath__]
         for directory in dirs:
             create_dirs(self.root + directory)
 
@@ -199,13 +191,77 @@ class BlobStore(object):
             byte_seq = ByteSequence(Sha1Lookup.get_sha1(blob_name), os.path.getsize(path))
             self.blobs.update({blob_name : byte_seq})
 
+class PersistentBlobStore(object):
+    """ Persistent BlobStore that saves blob data to a JSON file. """
+    __metapath__ = 'meta/'
+    __blobmeta__ = __metapath__ + 'blob.json'
+
+    def __init__(self, root):
+        self.blobstore = BlobStore(root)
+        self.initialise()
+
+    def get_blob(self, sha1):
+        """ Get a blob by it's key. """
+        return self.blobstore.get_blob(sha1)
+
+    def get_blob_count(self):
+        """ Returns the number of blobs in the store. """
+        return self.blobstore.get_blob_count()
+
+    def get_size(self):
+        """ Returns the number of bytes in the store. """
+        return self.blobstore.get_size()
+
+    def add_file(self, path, sha1=None):
+        """ Add a file to the blobstore with optional SHA1 check. """
+        return self.blobstore.add_file(path, sha1)
+
+    def initialise(self):
+        """ If persist_to exists, tries to load a serialised lookup table from it.
+        Populates lookup table and saves to persist_to if persist_to doesn't exist.
+        """
+
+        persist_to = self.get_blob_meta_path()
+        if os.path.isfile(persist_to):
+            # Persistence file exists, load the dictionary
+            with open(persist_to, 'r') as lookup_file:
+                self.load(lookup_file)
+        else:
+            with open(persist_to, 'w+') as lookup_file:
+                self.save(lookup_file)
+
+    def get_blob_meta_path(self):
+        """ Returns the full path of the Blob metadata file. """
+        return self.blobstore.get_root() + self.__class__.__blobmeta__
+
+    def persist(self):
+        """ Persists the current state of the internal blobstore dictionary
+        to the default location, overwriting the existing state.
+        """
+        persist_to = self.get_blob_meta_path()
+        with open(persist_to, 'w+') as lookup_file:
+            self.save(lookup_file)
+
+    def save(self, dest):
+        """ Serialise the datacentre lookup dictionary to fp (a write() supporting
+        file-like object). """
+        json.dump(self.blobstore.blobs, dest, cls=ObjectJsonEncoder)
+
+    def load(self, src):
+        """ Loads the datacentre lookup dictionary from fp (a read() supporting
+        file like object)."""
+        blobs = json.load(src, object_hook=ByteSequence.json_decode)
+        self.blobstore.replace_blobs(blobs)
+
+
 def main():
     """
     Main method entry point, parses DOIs from Datacite and outputs to
     STDOUT.
     """
-    blob_store = BlobStore(BLOB_STORE_ROOT, persist=False)
-    print 'Blobstore contains {} blobs, {} bytes'.format(blob_store.get_blob_count(), blob_store.get_size())
+    blob_store = BlobStore(BLOB_STORE_ROOT)
+    print 'Blobstore contains {} blobs, {} bytes'.format(blob_store.get_blob_count(),
+                                                         blob_store.get_size())
 
 if __name__ == "__main__":
     main()
